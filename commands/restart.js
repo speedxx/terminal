8cb24c1348c66a905f0f4c9bc74b997893ad826d
squareGITHUB <44121785+squareGITHUB@users.noreply.github.com>
2018-12-20 16:45:11 +0000

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -5,8 +5,7 @@ module.exports.run = async (bot, message, args, client) => {
     if (message.author.id !== "372078453236957185")
     if (message.author.id !== "365274392680333329") return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "You are not a Terminal developer.")
     try {
-message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + 'Restarting')
-    .then(message => bot.destroy())
+message.channel.send("/" + message.guild + "/" + message.channel.name + "/ \n  " + 'Restarting...').then(message => bot.destroy())
     .then(() => bot.login(config.token))
 } catch (e) {
     mesage.channel.send(`${client.clean(err)}`)
