d9a726f82c914627382b8d9bd4f420a8db3d2388
squareGITHUB <44121785+squareGITHUB@users.noreply.github.com>
2018-12-20 16:18:23 +0000

    Create restart.js

---
+++ commands/restart.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const config = require("../config.json");
+
+module.exports.run = async (bot, message, args, client) => {
+    if (message.author.id !== "372078453236957185")
+    if (message.author.id !== "365274392680333329") return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "You are not a Terminal developer.")
+    try {
+message.channel.send('Restarting')
+    .then(message => bot.destroy())
+    .then(() => bot.login(config.token))
+} catch (e) {
+    mesage.channel.send(`${client.clean(err)}`)
+  }
+}
+module.exports.help = {
+    name: "restart"
+}
