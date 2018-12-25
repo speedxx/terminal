58ab1654277bf67560c18982498a127dbd4d3675
speed <43330808+jsinitx4@users.noreply.github.com>
2018-12-25 16:59:26 -0500

    Update roleinfo.js

--- commands/roleinfo.js
+++ commands/roleinfo.js
@@ -4,6 +4,8 @@ module.exports.run = async(client, message, args) => {
     if (args.includes("@everyone")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + ' **Error**');
     if (args.includes("@here")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + ' **Error** ');
     let role = message.mentions.roles.first() || message.guild.roles.get(args[0]) || message.guild.roles.find(r => r.name.toLowerCase() == args.join(' ').toLocaleLowerCase());
+    if (args.length < 1) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "Please supply a role name.")
+    if(!role) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " That role doesn't exist, if it does exist: check your spelling");
     let roleembed = new Discord.RichEmbed()
     .setAuthor("Role information for " + role.name)
     .setColor(Math.floor(Math.random() * 16777214) + 1)
