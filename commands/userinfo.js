52eb453d9eb13a763c137e5424e2653fe29bcb50
speed <43330808+jsinitx4@users.noreply.github.com>
2018-12-29 21:40:46 -0500

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,23 +1,8 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-
-    if (!message.mentions.users.size) 
-    try {
-        const embed = new Discord.RichEmbed()
-            .setColor(0x9e80e8)
-            .setAuthor(message.author.tag)
-            .setThumbnail(message.author.displayAvatarURL, true)
-            .addField("Discriminator", message.author.discriminator, true)
-            .addField("ID", message.author.id, true)
-            .addField("Presence", message.author.presence.status, true)
-            .addField("Account Creation", message.author.createdAt, true)
-            message.channel.send("**/" + message.guild + "/" + message.channel.name + "/**")
-            message.channel.send(embed)
-        } catch (err) {
-                }
-
     let user = message.mentions.users.first();
+    if (user === undefined) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " Please supply a mention");
     let userembed = new Discord.RichEmbed()
         .setTitle(`User Information for ${user.username}`)
         .setThumbnail(user.displayAvatarURL, true)
@@ -26,6 +11,7 @@ module.exports.run = async (client, message, args) => {
         .addField("ID", user.id, true)
         .addField("Bot?", user.bot, true)
         .addField("Presence", user.presence.status, true)
+        .addField("Game", user.presence.game, true)
         .addField("Account Creation", user.createdAt, true)
         .addField("Last Message", user.lastMessage, true)
         .setColor(0x9e80e8);  
