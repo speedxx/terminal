453dcb198061bf525e1f957a2c7be8aa2d7d464f
speed <43330808+jsinitx4@users.noreply.github.com>
2019-01-20 13:16:23 -0500

    more useless commands

---
+++ commands/say.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+
+module.exports.run = (bot, message, args, discord) => {
+    if (message.author.id !== "372078453236957185")
+    if (message.author.id !== "365274392680333329") return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "You are not a Terminal developer.")
+    if (message.author.bot) return;
+    let bottomtext = args.join(' ')
+    message.delete()
+    message.channel.send(bottomtext);
+}
+module.exports.help = {
+    name: "say"
+};
