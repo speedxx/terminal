afa8ea0283b4fbabd8abb1f35775eaa30b33c022
speed <43330808+jsinitx4@users.noreply.github.com>
2019-02-02 16:21:00 -0500

    new bot owner

--- commands/restart.js
+++ commands/restart.js
@@ -3,6 +3,7 @@ const config = require("../config.json");
 
 module.exports.run = async (bot, message, args, client) => {
     if (message.author.id !== "372078453236957185")
+    if (message.author.id !== "147765181903011840")
     if (message.author.id !== "365274392680333329") return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "You are not a Terminal developer.")
     try {
 message.channel.send("**/" + message.guild + "/" + message.channel.name + "/ \n  " + '**Restarting...').then(message => bot.destroy())
