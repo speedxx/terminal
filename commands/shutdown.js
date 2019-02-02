2484881666d081709fc043e990b5c59ed1dedfbc
speed <43330808+jsinitx4@users.noreply.github.com>
2019-02-02 16:21:40 -0500

    new bot owner

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -1,5 +1,6 @@
 module.exports.run = async (bot, message, args, client) => {
   if (message.author.id !== "372078453236957185")
+  if (message.author.id !== "147765181903011840")
     if (message.author.id !== "365274392680333329") return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "You are not a Terminal developer.")
     try {
     console.log("Shutdown successfully.")
