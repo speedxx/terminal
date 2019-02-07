7b35beeeb0ed516e5d47fd971aa563ee1e14bc3b
speed <43330808+jsinitx4@users.noreply.github.com>
2019-02-07 15:22:16 -0500

    Delete host.js

--- commands/host.js
+++
@@ -1,7 +0,0 @@
-const Discord = require("discord.js");
-module.exports.run = async (client, message, args) => {
-    message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "The current host is: **host**")
-    }
-module.exports.help = {
-    name: "host"
-}
