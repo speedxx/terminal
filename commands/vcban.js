234e73efa07a18ffe7a9d43104ddace55abf0b15
squareGITHUB <44121785+squareGITHUB@users.noreply.github.com>
2019-02-16 20:37:36 +0000

    b

--- commands/vcban.js
+++ commands/vcban.js
@@ -2,25 +2,24 @@ const Discord = require("discord.js");
 const ms = require("ms");
 
 module.exports.run = async (bot, message, args) => {
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " You do not have sufficient permissions to blind members.");
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " You do not have sufficient permissions to vc ban members.");
     if (args.includes("@everyone")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + 'Error.');
     if (args.includes("@here")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + 'Error.');
     let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if (!tomute) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "Couldn't find user.");
-    if (tomute.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "The user you are trying to blind is either the same, or higher ranking than you.");
-    let muterole = message.guild.roles.find(`name`, "Blinded");
+    if (tomute.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "The user you are trying to vc ban is either the same, or higher ranking than you.");
+    let muterole = message.guild.roles.find(`name`, "VC Banned");
 
     if (!muterole) {
         try {
             muterole = await message.guild.createRole({
-                name: "Blinded",
+                name: "VC Banned",
                 color: "#000000",
                 permissions: []
             })
             message.guild.channels.forEach(async (channel, id) => {
                 await channel.overwritePermissions(muterole, {
-                    VIEW_CHANNEL: false,
-                    SEND_MESSAGES: false
+                     CONNECT: false,
                 });
             });
         } catch (e) {
@@ -32,16 +31,16 @@ module.exports.run = async (bot, message, args) => {
     if (!mutetime) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "Please specify a time.");
 
     await (tomute.addRole(muterole.id));
-    message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + `<@${tomute.id}> has been blinded for ${ms(ms(mutetime))}`);
+    message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + `<@${tomute.id}> has been VC banned for ${ms(ms(mutetime))}`);
 
     setTimeout(function() {
         if(!tomute.roles.has(muterole.id)) return 
         tomute.removeRole(muterole.id);
-        message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + `<@${tomute.id}> has been unblinded.`);
+        message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + `<@${tomute.id}> has been unvc banned.`);
     }, ms(mutetime));
 
 }
 
 module.exports.help = {
-    name: "blind",
+    name: "vcban",
 }
