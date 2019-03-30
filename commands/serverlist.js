cd6d754c220233009bbfa640ccd7ecf3d90e17b8
speed <43330808+jsinitx4@users.noreply.github.com>
2019-03-30 19:48:43 -0400

    fix

--- commands/serverlist.js
+++ commands/serverlist.js
@@ -1,7 +1,6 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " +  bot.guilds.map(r => " **" + r.name + "** with " + r.memberCount + " members"))
+module.exports.run = async (bot, message) => {
+let guilds = bot.guilds.filter(g => g.memberCount > 100).map(r => "**" + r.name + "**" + " with " + r.memberCount + " members").join("\n");
+message.channel.send("**/" + message.guild + "/" + message.channel.name + "/**" + "\n" + guilds)  
 }
 module.exports.help = {
     name: "serverlist"
