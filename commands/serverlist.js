f12aec935d5af0ed7c9640f61b68f248e3110163
squareGITHUB <44121785+squareGITHUB@users.noreply.github.com>
2019-03-31 12:03:46 +0100

    Update serverlist.js

--- commands/serverlist.js
+++ commands/serverlist.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " +  bot.guilds.map(r => " **" + r.name + "** with " + r.memberCount + " members"))
+message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " +  bot.guilds.map(r => " **" + r.name + "** with " + r.memberCount + " members").sort((a, b) => b - a).slice(0,5))
 }
 module.exports.help = {
     name: "serverlist"
