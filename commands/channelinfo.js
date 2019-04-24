7a9e762e4cd2ec0bf324c30124aa5222816a4f8d
speed <43330808+jsinitx4@users.noreply.github.com>
2019-04-24 14:31:04 -0400

    ok discord

--- commands/channelinfo.js
+++ commands/channelinfo.js
@@ -1,18 +1,18 @@
 const Discord = require("discord.js");
 
 module.exports.run = (bot, message, args) => {
+    let channel = message.channel
     let channelinfo = new Discord.RichEmbed()
     .setColor(Math.floor(Math.random() * 16777214) + 1)
-    .setTitle("Channel Information for " + message.channel.name)
-    .addField("NSFW?", message.channel.nsfw)
-    .addField("Channel ID", message.channel.id)
-    .addField("Channel type", message.channel.type)
-    .addField("Channel description", message.channel.topic)
-    .addField("Channel creation", message.channel.createdAt)
+    .setTitle("Channel Information for " + channel.name)
+    .addField("NSFW?", channel.nsfw)
+    .addField("Channel ID", channel.id)
+    .addField("Channel type", channel.type)
+    .addField("Channel description", channel.topic)
+    .addField("Channel creation", channel.createdAt)
  message.channel.send("**/" + message.guild + "/" + message.channel.name + "/**")
  message.channel.send(channelinfo);
 }
   module.exports.help = {
     name: "channelinfo"
 }
-  
