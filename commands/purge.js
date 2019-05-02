2b597cfc13fb20863cba78e769b2a44b9250ccbb
jsinitx4 <d.kilmetov@gmail.com>
2019-05-02 16:11:53 -0400

    ok

--- commands/purge.js
+++ commands/purge.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = (client, message, args) => {
   if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " You do not have sufficient permissions to purge messages.");
   if(!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " I do not have sufficient permissions to manage messages.");
-  if(isNaN(args[0])) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " Supply an interger please");
+  if(isNaN(args[0])) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " Supply an number please");
   if (args[0] > 100) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " I can't purge more than 100 messages.");
   message.channel.bulkDelete(args[0]);
   message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " Successfully deleted " + args[0] + " messages.")
