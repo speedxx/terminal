24ee2a9a84cd3b62099669c137719bd5d50e7d87
speed <43330808+jsinitx4@users.noreply.github.com>
2019-12-23 10:08:04 -0500

    fix a lot

--- commands/blind.js
+++ commands/blind.js
@@ -3,80 +3,96 @@ const ms = require("ms");
 const fs = require("fs")
 
 module.exports.run = async (bot, message, args, client) => {
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " You do not have sufficient permissions to blind members.");
-    if (args.includes("@everyone")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + 'Error.');
-    if (args.includes("@here")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + 'Error.');
-    if(!message.guild.me.hasPermission("MANAGE_ROLES")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " I do not have sufficient permissions to add roles.");
-    let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if (!tomute) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "Couldn't find user.");
-    if (tomute === message.author) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "You cannot blind yourself.");
-    if (tomute.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "The user you are trying to blind is either the same, or higher ranking than you.");
-    let muterole = message.guild.roles.find(`name`, "Blinded");
+  let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  let muterole = message.guild.roles.find(`name`, "Blinded");
 
-    if (!muterole) {
-        try {
-            muterole = await message.guild.createRole({
-                name: "Blinded",
-                color: "#000000",
-                permissions: []
-            })
-            message.guild.channels.forEach(async (channel, id) => {
-                await channel.overwritePermissions(muterole, {
-                    VIEW_CHANNEL: false,
-                    SEND_MESSAGES: false
-                });
-            });
-        } catch (e) {
-            console.log(e.stack);
-        }
+  if (!message.member.hasPermission("MANAGE_MESSAGES"))
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " You do not have sufficient permissions to blind members.");
+
+  if (args.includes("@everyone"))
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + 'Error.');
+
+  if (args.includes("@here"))
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + 'Error.');
+
+  if (!message.guild.me.hasPermission("MANAGE_ROLES"))
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " I do not have sufficient permissions to add roles.");
+
+  if (!tomute)
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "Couldn't find user.");
+
+  if (tomute === message.author)
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "You cannot blind yourself.");
+
+  if (tomute.hasPermission("MANAGE_MESSAGES"))
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "The user you are trying to blind is either the same, or higher ranking than you.");
+
+  if (!muterole) {
+    try {
+      muterole = await message.guild.createRole({
+        name: "Blinded",
+        color: "#000000",
+        permissions: []
+      })
+      message.guild.channels.forEach(async (channel, id) => {
+        await channel.overwritePermissions(muterole, {
+          VIEW_CHANNEL: false,
+          SEND_MESSAGES: false,
+          READ_MESSAGE_HISTORY: false
+        });
+      });
+    } catch (e) {
+      console.log(e.stack);
     }
+  }
 
-    let mutetime = args[1];
-    if (!mutetime) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "Please specify a time.");
+  let mutetime = args[1];
+  if (!mutetime)
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "Please specify a time.");
 
-    await (tomute.addRole(muterole.id));
-    message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + `<@${tomute.id}> has been blinded for ${ms(ms(mutetime))}`);
-    let logs = JSON.parse(fs.readFileSync("./logs.json", "utf8"));
-    if (!logs[message.guild.id]) { 
-      logs[message.guild.id] = {
-        toggle: 0
-      };
-    } 
-    if (logs[message.guild.id].toggle === 1) {
-      const logchannel = message.guild.channels.find(channel => channel.name === "terminal-logs");
-      let eventembed = new Discord.RichEmbed()
+  await (tomute.addRole(muterole.id));
+  message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + `<@${tomute.id}> has been blinded for ${ms(ms(mutetime))}`);
+  let logs = JSON.parse(fs.readFileSync("./logs.json", "utf8"));
+  if (!logs[message.guild.id]) {
+    logs[message.guild.id] = {
+      toggle: 0
+    };
+  }
+  if (logs[message.guild.id].toggle === 1) {
+    const logchannel = message.guild.channels.find(channel => channel.name === "terminal-logs");
+    let eventembed = new Discord.RichEmbed()
       .setColor(0xff0000)
       .setTitle("Blind Event:")
       .addField("User Blinded:", tomute)
       .addField("Time:", mutetime)
       .addField("Admin:", message.author)
       .setTimestamp()
-   logchannel.send(eventembed);
-    }
+    logchannel.send(eventembed);
+  }
 
-    setTimeout(function() {
-        if(!tomute.roles.has(muterole.id)) return 
-        tomute.removeRole(muterole.id);
-        message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + `<@${tomute.id}> has been unblinded.`);
-        let logs = JSON.parse(fs.readFileSync("./logs.json", "utf8"));
-        if (!logs[message.guild.id]) { 
-          logs[message.guild.id] = {
-            toggle: 0
-          };
-        } 
-        if (logs[message.guild.id].toggle === 1) {
-          const logchannel = message.guild.channels.find(channel => channel.name === "terminal-logs");
-          let eventembed = new Discord.RichEmbed()
-          .setColor(0x00ff00)
-          .setTitle("Unblind Event:")
-          .addField("User Unblinded:", rMember)
-          .addField("Admin:", message.author)
-          .setTimestamp()
-       logchannel.send(eventembed);
-        }
-    }, ms(mutetime));
+  setTimeout(function () {
+    if (!tomute.roles.has(muterole.id)) return
+    tomute.removeRole(muterole.id);
+    message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + `<@${tomute.id}> has been unblinded.`);
+    let logs = JSON.parse(fs.readFileSync("./logs.json", "utf8"));
+    if (!logs[message.guild.id]) {
+      logs[message.guild.id] = {
+        toggle: 0
+      };
+    }
+    if (logs[message.guild.id].toggle === 1) {
+      const logchannel = message.guild.channels.find(channel => channel.name === "terminal-logs");
+      let eventembed = new Discord.RichEmbed()
+        .setColor(0x00ff00)
+        .setTitle("Unblind Event:")
+        .addField("User Unblinded:", tomute)
+        .addField("Admin:", message.author)
+        .setTimestamp()
+      logchannel.send(eventembed);
+    }
+  }, ms(mutetime));
 }
 
 module.exports.help = {
-    name: "blind",
+  name: "blind",
 }
