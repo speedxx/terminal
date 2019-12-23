335204d8adeea2d8b4068b4930ac41b1076bcd54
speed <43330808+jsinitx4@users.noreply.github.com>
2019-12-22 19:40:16 -0500

    actually fix this

--- commands/addrole.js
+++ commands/addrole.js
@@ -2,43 +2,60 @@ const Discord = require("discord.js")
 const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
-  if (args.includes("@everyone")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + ' **Error**');
-  if (args.includes("@here")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + ' **Error** '); 
-  if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " You do not have sufficient permissions to add roles.");
-  if(!message.guild.me.hasPermission("MANAGE_ROLES")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " I do not have sufficient permissions to manage roles.");
+
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  if(!rMember) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n " + "  Couldn't find that user");
   let role = args.join(" ").slice(22);
-  if(!role) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " Please supply a role");
-  let gRole = message.guild.roles.find(`name`, role);
-  if(!gRole) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " That role doesn't exist, if it does exist: check your spelling");
+  let gRole = message.guild.roles.find(r => r.name == args[1]) || message.guild.roles.find(r => r.id == args[1]) || message.mentions.roles.first()
 
-  if(rMember.roles.has(gRole.id)) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " This person already has that role.");
-  await(rMember.addRole(gRole.id));
+  if (args.includes("@everyone"))
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + ' **Error**');
 
-  try{
+  if (args.includes("@here"))
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + ' **Error** ');
+
+  if (!message.member.hasPermission("MANAGE_ROLES"))
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " You do not have sufficient permissions to add roles.");
+
+  if (!message.guild.me.hasPermission("MANAGE_ROLES"))
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " I do not have sufficient permissions to manage roles.");
+
+  if (!rMember)
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n " + "  Couldn't find that user");
+
+  if (!role)
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " Please supply a role");
+
+  if (!gRole)
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " That role doesn't exist, if it does exist: check your spelling");
+
+  if (rMember.roles.has(gRole.id))
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " This person already has that role.");
+
+  await (rMember.addRole(gRole.id));
+
+  try {
     await message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + ` Added **${gRole.name}** to ${rMember}.`)
     let logs = JSON.parse(fs.readFileSync("./logs.json", "utf8"));
-    if (!logs[message.guild.id]) { 
+    if (!logs[message.guild.id]) {
       logs[message.guild.id] = {
         toggle: 0
       };
-    } 
+    }
     if (logs[message.guild.id].toggle === 1) {
       const logchannel = message.guild.channels.find(channel => channel.name === "terminal-logs");
       let eventembed = new Discord.RichEmbed()
-      .setColor(0x00ff00)
-      .setTitle("Add Role Event:")
-      .addField("User:", rMember)
-      .addField("Admin:", message.author)
-      .addField("Role:", gRole)
-      .setTimestamp()
-   logchannel.send(eventembed);
+        .setColor(0x00ff00)
+        .setTitle("Add Role Event:")
+        .addField("User:", rMember)
+        .addField("Admin:", message.author)
+        .addField("Role:", gRole)
+        .setTimestamp()
+      logchannel.send(eventembed);
     }
-  }catch(e){
+  } catch (e) {
     console.log(e.stack);
   }
 }
-  module.exports.help = {
-    name: "addrole"
+module.exports.help = {
+  name: "addrole"
 }
