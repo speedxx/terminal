851a804ead70942035a74eb41777e56df2d28e6d
speed <43330808+jsinitx4@users.noreply.github.com>
2019-12-22 21:31:29 -0500

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,22 +1,14 @@
 const Discord = require("discord.js");
 module.exports.run = async (client, msg, args) => {
+    let user = msg.mentions.users.first() || msg.author
     
-if (!msg.mentions.users.size) {
-const embed = new Discord.RichEmbed()
-    .setColor(Math.floor(Math.random() * 16777214) + 1)
-    .setAuthor(msg.author.tag)
-    .setImage(msg.author.displayAvatarURL)
-    msg.channel.send("**/" + msg.guild + "/" + msg.channel.name + "/**")
-    return msg.channel.send(embed);
-    }
-let user = msg.mentions.users.first() || msg.author
-    const dembed = new Discord.RichEmbed()
-    .setColor(Math.floor(Math.random() * 16777214) + 1)
-    .setAuthor(user.tag)
-    .setImage(user.displayAvatarURL)
+    let dembed = new Discord.RichEmbed()
+        .setColor(Math.floor(Math.random() * 16777214) + 1)
+        .setAuthor(user.tag)
+        .setImage(user.displayAvatarURL)
     msg.channel.send("**/" + msg.guild + "/" + msg.channel.name + "/**")
     return msg.channel.send(dembed);
-    }
+}
 module.exports.help = {
     name: "avatar"
 }
