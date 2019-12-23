fce2d239681da4f12db0af12579eedccea3fefb3
speed <43330808+jsinitx4@users.noreply.github.com>
2019-12-22 19:46:41 -0500

    another fix

--- commands/announce.js
+++ commands/announce.js
@@ -1,16 +1,20 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " You do not have sufficient permissions to announce in a channel.");
-    if (!args || args.length < 1) {
-        message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "Please put an announcement channel and a message.")
+    let announceChannel = message.mentions.channels.first();
+
+    if (!message.member.hasPermission("MANAGE_MESSAGES"))
+        return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " You do not have sufficient permissions to announce in a channel.");
+
+    if (!args || args.length < 2) {
+        return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "Please put an announcement channel and a message.")
     }
+
     let embedmsg = args.splice(1).join(' ');
     let announceembed = new Discord.RichEmbed()
-    .setColor(Math.floor(Math.random() * 16777214) + 1)
-    .setAuthor('Announcement from: ' + message.author.username)
-    .setDescription(embedmsg);
-    let announceChannel = message.mentions.channels.first();
+        .setColor(Math.floor(Math.random() * 16777214) + 1)
+        .setAuthor('Announcement from: ' + message.author.username)
+        .setDescription(embedmsg);
     message.guild.channels.find(t => t.id == announceChannel.id).send("**/" + message.guild + "/" + message.channel.name + "/**")
     message.guild.channels.find(t => t.id == announceChannel.id).send(announceembed);
 }
