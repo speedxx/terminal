e0ee46cc89abf8fa22e696ce765e5f85a8bf1c62
speed <43330808+jsinitx4@users.noreply.github.com>
2019-12-23 10:15:23 -0500

    Update censor.js

--- commands/censor.js
+++ commands/censor.js
@@ -1,10 +1,14 @@
 const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
-	if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "You do not have sufficient permissions to add censorship on words.");
-	if(!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " I do not have sufficient permissions to delete messages.");
+	if (!message.member.hasPermission("MANAGE_MESSAGES"))
+		return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "You do not have sufficient permissions to add censorship on words.");
+
+	if (!message.guild.me.hasPermission("MANAGE_MESSAGES"))
+		return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " I do not have sufficient permissions to delete messages.");
+
 	let censor = JSON.parse(fs.readFileSync("./censor.json", "utf8"));
-	if (!args[0]) { 
+	if (!args[0]) {
 		censor[message.guild.id] = {
 			toggle: 0
 		};
@@ -13,7 +17,8 @@ module.exports.run = async (bot, message, args) => {
 		});
 		message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "Cleared all censorship of words.");
 	}
-	if (args[0]) { 
+
+	if (args[0]) {
 		censor[message.guild.id] = {
 			toggle: 1
 		};
@@ -24,5 +29,5 @@ module.exports.run = async (bot, message, args) => {
 	}
 }
 module.exports.help = {
-    name: "censor"
+	name: "censor"
 }
