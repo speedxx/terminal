a0ef0f992c0a584d5b49219f76a56e974b5314fb
speed <43330808+jsinitx4@users.noreply.github.com>
2019-12-23 10:26:31 -0500

    Update channelinfo.js

--- commands/channelinfo.js
+++ commands/channelinfo.js
@@ -1,18 +1,18 @@
 const Discord = require("discord.js");
 
-module.exports.run = (bot, message, args) => {
-    let channel = message.channel
-    let channelinfo = new Discord.RichEmbed()
-    .setColor(Math.floor(Math.random() * 16777214) + 1)
-    .setTitle("Channel Information for " + channel.name)
+module.exports.run = async (client, message, args) => {
+  let channel = message.mentions.channels.first() || message.channel;
+  let channelembed = new Discord.RichEmbed()
+    .setTitle(`Channel Information for #${channel.name}`)
     .addField("NSFW?", channel.nsfw)
     .addField("Channel ID", channel.id)
     .addField("Channel type", channel.type)
     .addField("Channel description", channel.topic)
     .addField("Channel creation", channel.createdAt)
- message.channel.send("**/" + message.guild + "/" + message.channel.name + "/**")
- message.channel.send(channelinfo);
+    .setColor(Math.floor(Math.random() * 16777214) + 1)
+  message.channel.send("**/" + message.guild + "/" + message.channel.name + "/**")
+  return message.channel.send(channelembed);
 }
-  module.exports.help = {
-    name: "channelinfo"
+module.exports.help = {
+  name: "channelinfo"
 }
