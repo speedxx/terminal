e6c69f743606d877e042b84db9b034d59f97402c
speed <43330808+speedxx@users.noreply.github.com>
2020-04-06 14:40:07 -0400

    update to v12

--- commands/announce.js
+++ commands/announce.js
@@ -4,19 +4,19 @@ module.exports.run = async (client, message, args) => {
     let announceChannel = message.mentions.channels.first();
 
     if (!message.member.hasPermission("MANAGE_MESSAGES"))
-        return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " You do not have sufficient permissions to announce in a channel.");
+        return message.channel.send("**/" + message.guild.name + "/" + message.channel.name + "/** \n  " + " You do not have sufficient permissions to announce in a channel.");
 
     if (!args || args.length < 2) {
-        return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "Please put an announcement channel and a message.")
+        return message.channel.send("**/" + message.guild.name + "/" + message.channel.name + "/** \n  " + "Please put an announcement channel and a message.")
     }
 
     let embedmsg = args.splice(1).join(' ');
-    let announceembed = new Discord.RichEmbed()
+    let announceembed = new Discord.MessageEmbed()
         .setColor(Math.floor(Math.random() * 16777214) + 1)
         .setAuthor('Announcement from: ' + message.author.username)
         .setDescription(embedmsg);
-    message.guild.channels.find(t => t.id == announceChannel.id).send("**/" + message.guild + "/" + message.channel.name + "/**")
-    message.guild.channels.find(t => t.id == announceChannel.id).send(announceembed);
+    message.guild.channels.cache.find(t => t.id == announceChannel.id).send("**/" + message.guild.name + "/" + message.channel.name + "/**")
+    message.guild.channels.cache.find(t => t.id == announceChannel.id).send(announceembed);
 }
 module.exports.help = {
     name: "announce"
