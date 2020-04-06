c19b535c27b57b41b487262269fcceff3966fc43
speed <43330808+speedxx@users.noreply.github.com>
2020-04-06 14:27:13 -0400

    update to v12

--- commands/activity.js
+++ commands/activity.js
@@ -4,28 +4,28 @@ module.exports.run = (client, message, args) => {
     if (message.author.id !== "372078453236957185")
     if (message.author.id !== "147765181903011840")
     if (message.author.id !== "365274392680333329")
-    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "You are not a Terminal developer.")
+    return message.channel.send("**/" + message.guild.name + "/" + message.channel.name + "/** \n  " + "You are not a Terminal developer.")
 
     switch (args[0]) {
         case 'p': //setting activity to "playing"
-            client.user.setActivity(args.splice(1).join(' '), { type: 'playing' });
-            message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + '**Playing** status ready.');
+            client.user.setActivity(args.splice(1).join(' '), { type: 'PLAYING' });
+            message.channel.send("**/" + message.guild.name + "/" + message.channel.name + "/** \n  " + '**Playing** status ready.');
             break;
         case 'w': //setting activity to "watching"
-            client.user.setActivity(args.splice(1).join(' '), { type: 'watching' });
-            message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + '**Watching** status ready.')
+            client.user.setActivity(args.splice(1).join(' '), { type: 'WATCHING' });
+            message.channel.send("**/" + message.guild.name + "/" + message.channel.name + "/** \n  " + '**Watching** status ready.')
             break;
         case 'l': //setting activity to "listening"
-            client.user.setActivity(args.splice(1).join(' '), { type: 'listening' });
-            message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + '**Listening** status ready.');
+            client.user.setActivity(args.splice(1).join(' '), { type: 'LISTENING' });
+            message.channel.send("**/" + message.guild.name + "/" + message.channel.name + "/** \n  " + '**Listening** status ready.');
             break;
         case 's': //setting activity to "streaming"
-            client.user.setActivity(args.splice(1).join(' '), { type: "STREAMING", url: "https://www.twitch.tv/monstercat" });
-            message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + '**Streaming** status ready.');
+            client.user.setActivity(args.splice(1).join(' '), { type: 'STREAMING', url: "https://www.twitch.tv/monstercat" });
+            message.channel.send("**/" + message.guild.name + "/" + message.channel.name + "/** \n  " + '**Streaming** status ready.');
             break;
         case 'default': // resets current activity to config activity
             client.user.setActivity(config.activity, { type: config.status });
-            message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + 'Reset activity');
+            message.channel.send("**/" + message.guild.name + "/" + message.channel.name + "/** \n  " + 'Reset activity');
             return;
     }
 }
