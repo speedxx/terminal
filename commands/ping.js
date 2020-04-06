8f50cfe994316da1f2c78fc1493f5eedc4226fc6
speed <43330808+speedxx@users.noreply.github.com>
2020-04-06 14:51:38 -0400

    update to v12

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message, args) => {
     let pings = ["Loading...", "Waiting...", "Pinging...", "Doing a thing...", "Debugging...", "Shopping...", "Coding...", "Changing language...", "Doing nothing...", "Doing something...", "Doing things...", ":ok_hand:...", "Alright...", "Uh...", "404..."]
     var ping = pings[Math.floor(Math.random() * pings.length)];
     message.channel.send(ping).then((message) => {
-        message.edit("**/" + message.guild + "/" + message.channel.name + "/** \n  " + `Pinged! // \`${client.pings[0]}ms\``);
+        message.edit("**/" + message.guild.name + "/" + message.channel.name + "/** \n  " + `Pinged! // \`${Math.round(client.ws.ping)}ms\``);
     })
 };
 
