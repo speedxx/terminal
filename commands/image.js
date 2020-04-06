cf8f9aeccf07f17c1de9d27fce2753b0ceb05f80
speed <43330808+speedxx@users.noreply.github.com>
2020-04-06 18:45:12 -0400

    update to v12

--- commands/image.js
+++ commands/image.js
@@ -2,12 +2,12 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     if (!message.member.hasPermission("MANAGE_MESSAGES"))
-        return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "You do not have sufficient permissions to create an image embed.");
+        return message.channel.send("**/" + message.guild.name + "/" + message.channel.name + "/** \n  " + "You do not have sufficient permissions to create an image embed.");
 
     if (!args[0] || args[0].length < 1)
-        return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "Please put an Image URL to turn into an embed.")
+        return message.channel.send("**/" + message.guild.name + "/" + message.channel.name + "/** \n  " + "Please put an Image URL to turn into an embed.")
 
-    let imageembed = new Discord.RichEmbed()
+    let imageembed = new Discord.MessageEmbed()
         .setAuthor(`Image embed.`)
         .setImage(args[0])
         .setColor('RANDOM')
